e5bae5af886b29275cb325af49f8664a5b21a970
Kedric Webster <kedricwebster@gmail.com>
2017-08-08 18:14:47 -0400

    Additional problems

--- Dogs/scratch.js
+++ Dogs/scratch.js
@@ -1,4 +1,4 @@
-var puppyObject = [
+/* var puppyObject = [
   {
     name: 'Henry',
     age: 0.5,
@@ -23,7 +23,7 @@ var puppyObject = [
     toys: ['old sock', 'other old sock', 'more old socks'],
     picture: 'http://media.cmgdigital.com/shared/img/photos/2014/08/01/5a/66/LadyLabrador.jpg'
 }
-]
+] */
 
 /*for (var i = 0; i < puppyObject.length; i++) {  //includes starting point; conditional to end; and something to do at the end
   console.log(puppyObject[i].name)
@@ -53,15 +53,26 @@ var img ="http://media.cmgdigital.com/shared/img/photos/2014/08/01/5a/66/LadyLab
 $("body").append("<img src='" + img + "'/>");
 */
 
-for (var i = 0; i < puppyObject.length; i++) {
+/* for (var i = 0; i < puppyObject.length; i++) {
   $("body").append("<img src='" + puppyObject[i].picture + "'/>");
   $("body").append("<p>" + puppyObject[i].name + "</p>");
-}
+} */
 
-// Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3] without using `Array.concat`
+// Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3] without using `Array.concat` Problems from 8/1/17
 
-for (var i = 0; i < Array1; i++) {
-  for (var j = 0; j < Array2; j++) {
-    
+/* var a = ["a", "b", "c"];
+var b = [1, 2, 3];
+a.push.apply(a, b); */
+
+var a = ['a', 'b', 'c'];
+var b = [1, 2, 3];
+
+function merge(a, b) {
+  if (a.length == b.length) {
+    var c = [];
+    for (var i = 0; i < a.length; i++) {
+      c.push([a[i], b[i]]);
+    }
+  return c;
   }
-}
+} 
